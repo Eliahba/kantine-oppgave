a03265d6bc8aaf6ce60b45331068b22134383866
“Eliahba” <eliahba@gmail.com>
2024-11-04 12:37:35 +0100

    dynamisk oppdatering

---
+++ javaScript.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Eksempeldata for ukens meny og varer
+const ukensMeny = [
+    { dag: "Mandag", rett: "Spaghetti Bolognese", beskrivelse: "En klassisk italiensk rett.", pris: "40,-", bilde: "bilder/varmrett1.jpg" },
+    { dag: "Tirsdag", rett: "Fiskeboller", beskrivelse: "Serveres med hvit saus.", pris: "35,-", bilde: "bilder/varmrett2.jpg" },
+    // Legg til andre dager
+];
+
+const varerOgPriser = [
+    { navn: "Bagett", pris: "35,-", bilde: "bilder/vare1.jpg" },
+    { navn: "Mineralvann", pris: "25,-", bilde: "bilder/vare2.jpg" },
+    // Legg til flere varer
+];
+
+// Funksjoner for å laste dataene på sidene (brukes i meny.html og varer.html)
